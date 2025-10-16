c7e091b162ed53ece31d7a6db0a5e7e58ea1c218
Thor-droid <10512525@ucn.dk>
2025-10-16 15:41:38 +0200

    Lavede meget lidt på orderDB

--- src/dal/OrderDB.java
+++ src/dal/OrderDB.java
@@ -1,5 +1,25 @@
 package dal;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class OrderDB implements OrderDAO {
+	
+	
+	private static final String PS_INSERT = "INSERT INTO Order VALUES (?, ?, ?, ?, ?, ?)";
+	private static final String PS_SELECT_BY_ID = "SELECT * FROM Order WHERE CustomerNo = ?";
+	private static final String PS_SELECT = "SELECT * FROM Order";
+	private static final String PS_UPDATE = "UPDATE Order SET OrderNo = ?, items = ?, date = ?, amount = ?, deliveryStatus = ? WHERE deliveryDate = ?";
+	private static final String PS_DELETE = "DELETE FROM Customer WHERE CustomerNo = ?";
+	private static final String PS_SELECT_PRIVATE = "SELECT * FROM PrivateCustomer WHERE cvr = ?";
+	private static final String PS_SELECT_BUSINESS = "SELECT * FROM BusinessCustomer WHERE email = ?";
+	
+	private PreparedStatement insertPS;
+	private PreparedStatement selectByIdPS;
+	private PreparedStatement selectPS;
+	private PreparedStatement updatePS;
+	private PreparedStatement deletePS;
+	private PreparedStatement selectPrivatePS;
+	private PreparedStatement selectBusinessPS;
 
 }
