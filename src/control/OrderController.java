064b3cbe5ea0f2695a176b0752e4623dd8577479
Felix M. <felix.benjamin.mathiassen@gmail.com>
2025-10-16 16:29:42 +0200

    main

--- src/control/OrderController.java
+++ src/control/OrderController.java
@@ -1,5 +1,75 @@
 package control;
 
+import model.Customer;
+import model.Order;
+import model.Orderline;
+import model.Product;
+
 public class OrderController {
+	private Order tempOrder;
+	
+	public OrderController() {
+		
+	}
+	
+	public void newOrder() {
+		tempOrder = new Order();
+	}
+	
+	public boolean newOrderline(Product p, int qty) {
+		/*set false
+		 * Check stock
+		 * if available new Orderline
+		 * set true
+		 * tempOrder.addOrderline
+		 * 
+		 */
+		
+		return true;
+	}
+	
+	public void addCustomer(Customer c) {
+		tempOrder.setCustomer(c);
+	}
+	
+	/*
+	 * New order
+	 * Customer
+	 * LOOP
+	 * Add Product
+	 * 		check stock
+	 * 		if enough	
+	 * 			add Orderline to Order
+	 * 			update reserved in stock
+	 *#/LOOP
+	 * Apply discount
+	 * Delivery date
+	 * total amount
+	 * confirm
+	 * Set Status 
+	 * save order
+	 * 
+	 * #Start Transaction#
+	 * 
+	 * save order to db
+	 * get orderID
+	 * save orderlines to db with orderID
+	 * 
+	 * Commit || Rollback
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+	
+
+	
+	public boolean saveOrder(Order o) {
+		//OrderDB.saveOrder() #TODO
+		return false;
+		
+	}
 
 }
