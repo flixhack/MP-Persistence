a1afb04f576c07abbe4a8df8dc6df956416870c2
Thor-droid <10512525@ucn.dk>
2025-10-17 12:53:24 +0200

    CustomerController + main

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -19,11 +19,19 @@ public class Main {
     public static void main(String[] args) {
         Main main = new Main();
         main.tryCreatePrivateCustomer();
+        main.tryCreateBusinessCustomer();
+    }
+    private void tryCreatePrivateCustomer() {
+		try {
+			Customer c = cc.createPrivateCustomer("Gibe", "Sofiendalsvej", 9000, "Ålleren", 112, "awd@awdk.dk");
+			} catch (DataAccessException e) {
+				e.printStackTrace();
+			}
     }
 
-    private void tryCreatePrivateCustomer() {
+    private void tryCreateBusinessCustomer() {
         try {
-            Customer s = cc.createBusinessCustomer("gibe", "Sofiendalsvej", 9000, "Ålleren", "112", 123);
+            Customer s = cc.createBusinessCustomer("gibe", "Sofiendalsvej", 9000, "Ålleren", 112, 12345678);
         } catch (DataAccessException e) {
             e.printStackTrace();
         }
