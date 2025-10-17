bb442414ed8e25832dda97991e1873ae8c97f71b
Thor-droid <10512525@ucn.dk>
2025-10-17 12:37:19 +0200

    Jeg prøvede

--- src/control/CustomerController.java
+++ src/control/CustomerController.java
@@ -1,12 +1,42 @@
 package control;
 
+import java.util.List;
+
 import dal.CustomerDB;
 import dal.CustomerDAO;
 import dal.DataAccessException;
+import model.Customer;
+import model.PrivateCustomer;
 
 public class CustomerController {
+	private CustomerDAO customerDAO;
 	public CustomerController() throws DataAccessException {
-		CustomerDAO customerDAO;
 		customerDAO = new CustomerDB();
 	}
+	public Customer createCustomer(String name, String email) throws DataAccessException {
+		// Step 1: Check if already exists
+		Customer c = customerDAO.findPrivateByEmail(email);
+		if (c == null) {
+			// Step 2: If not, create and insert new PrivateCustomer
+			PrivateCustomer pc = new PrivateCustomer();
+			pc.setName(name);
+			pc.setEmail(email);
+			pc.setAddress("");
+			pc.setZipcode(0);
+			pc.setCity("");
+			pc.setPhoneNo(0);
+			// save to db
+			c = customerDAO.insert(pc);
+		}
+		return c;
+	}
+
+	public Customer findByEmail(String email) throws DataAccessException {
+		return customerDAO.findPrivateByEmail(email);
+	}
+
+	public List<Customer> getAll() throws DataAccessException {
+		return customerDAO.getAllCustomers();
+	}
+    
 }
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,10 +1,31 @@
 package ui;
 
+import control.CustomerController;
+import dal.DataAccessException;
+import model.Customer;
+
 public class Main {
+    private CustomerController cc;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public Main() {
+        try {
+            cc = new CustomerController();
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+            // handle exception as needed
+        }
+    }
 
-	}
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.tryCreateCustomer();
+    }
 
+    private void tryCreateCustomer() {
+        try {
+            Customer s = cc.createPrivateCustomer("123", "Gibe");
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+        }
+    }
 }
